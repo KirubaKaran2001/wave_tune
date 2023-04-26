4b2c9fd7ddbacc6b8db2e85a71cf982319a68377
karan@sbox.ltd <kirubap01@gmail.com>
2023-04-26 18:11:02 +0530

    sdk version increased

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wave_tune/screens/home_screen.dart';
@@ -11,7 +10,7 @@ import 'package:wave_tune/screens/splash_screen.dart';
 import 'constants/constants.dart';
 
 void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 //  await Firebase.initializeApp();
   prefs = await SharedPreferences.getInstance();
   // prefs!.clear();
